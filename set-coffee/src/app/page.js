e53b1b7647f14eb4792b5e4c160760620eecdf15
Hamed Babaei <dev.hamedbabaei@gmail.com>
2024-03-18 00:06:53 +0330

    build(Banner): build and style Banner component

--- set-coffee/src/app/page.js
+++ set-coffee/src/app/page.js
@@ -1,9 +1,11 @@
 import Navbar from "@/components/modules/navbar/Navbar";
+import Banner from "@/components/templates/index/banner/Banner";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Banner />
     </>
   );
 }
