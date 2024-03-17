b325f5b2dcf9b591cb97bcac941b9cda7930a6b6
Hamed Babaei <dev.hamedbabaei@gmail.com>
2024-03-18 00:33:34 +0330

    build(Promote and Articles): build and style Promote and Articles components

--- set-coffee/src/app/page.js
+++ set-coffee/src/app/page.js
@@ -1,6 +1,9 @@
+import Footer from "@/components/modules/footer/Footer";
 import Navbar from "@/components/modules/navbar/Navbar";
+import Articles from "@/components/templates/index/articles/Articles";
 import Banner from "@/components/templates/index/banner/Banner";
 import Latest from "@/components/templates/index/latest/Latest";
+import Promote from "@/components/templates/index/promote/Promote";
 
 export default function Home() {
   return (
@@ -8,6 +11,9 @@ export default function Home() {
       <Navbar />
       <Banner />
       <Latest />
+      <Promote />
+      <Articles />
+      <Footer />
     </>
   );
 }
