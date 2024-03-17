4fe36eb08309cc20b4cd5cd598cffb77add4de71
Hamed Babaei <dev.hamedbabaei@gmail.com>
2024-03-18 00:12:31 +0330

    build(Latest): build and style Latest component

--- set-coffee/src/app/page.js
+++ set-coffee/src/app/page.js
@@ -1,11 +1,13 @@
 import Navbar from "@/components/modules/navbar/Navbar";
 import Banner from "@/components/templates/index/banner/Banner";
+import Latest from "@/components/templates/index/latest/Latest";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Banner />
+      <Latest />
     </>
   );
 }
