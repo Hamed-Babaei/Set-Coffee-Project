16cd30c087822fcde558759584d7a96418ae7463
Hamed Babaei <dev.hamedbabaei@gmail.com>
2024-04-02 21:07:51 +0330

    build(page): build and style product page

---
+++ set-coffee/src/components/templates/product/Details.js
@@ -0,0 +1,91 @@
+import { FaFacebookF, FaStar, FaTwitter, FaRegStar } from "react-icons/fa";
+import { IoCheckmark } from "react-icons/io5";
+import { CiHeart } from "react-icons/ci";
+import { TbSwitch3 } from "react-icons/tb";
+import { FaTelegram, FaLinkedinIn, FaPinterest } from "react-icons/fa";
+import styles from "./details.module.css";
+import Breadcrumb from "./Breadcrumb";
+
+const Details = ({ product }) => {
+  return (
+    <main style={{ width: "63%" }}>
+      <Breadcrumb title={product.name} />
+      <h2>{product.name}</h2>
+
+      <div className={styles.rating}>
+        <div>
+          {new Array(product.score).fill(0).map((item, index) => (
+            <FaStar key={index} />
+          ))}
+
+          {new Array(5 - product.score).fill(0).map((item, index) => (
+            <FaRegStar key={index} />
+          ))}
+        </div>
+        <p>(دیدگاه {product.comments.length} کاربر)</p>
+      </div>
+
+      <p className={styles.price}>{product.price.toLocaleString()} تومان</p>
+      <span className={styles.description}>{product.shortDescription}</span>
+
+      <hr />
+
+      <div className={styles.Available}>
+        <IoCheckmark />
+        <p>موجود در انبار</p>
+      </div>
+
+      <div className={styles.cart}>
+        <button>افزودن به سبد خرید</button>
+        <div>
+          <span>-</span>1<span>+</span>
+        </div>
+      </div>
+
+      <section className={styles.wishlist}>
+        <div>
+          <CiHeart />
+          <a href="/">افزودن به علاقه مندی ها</a>
+        </div>
+        <div>
+          <TbSwitch3 />
+          <a href="/">مقایسه</a>
+        </div>
+      </section>
+
+      <hr />
+
+      <div className={styles.details}>
+        <strong>شناسه محصول: {product._id}</strong>
+
+        <p>
+          <strong>برچسب:</strong>
+          {product.tags.join(" ,")}
+        </p>
+      </div>
+
+      <div className={styles.share}>
+        <p>به اشتراک گذاری: </p>
+        <a href="/">
+          <FaTelegram />
+        </a>
+        <a href="/">
+          <FaLinkedinIn />
+        </a>
+        <a href="/">
+          <FaPinterest />
+        </a>
+        <a href="/">
+          <FaTwitter />
+        </a>
+        <a href="/">
+          <FaFacebookF />
+        </a>
+      </div>
+
+      <hr />
+    </main>
+  );
+};
+
+export default Details;
---
+++ set-coffee/src/components/templates/product/Tabs.js
@@ -0,0 +1,75 @@
+"use client";
+import React from "react";
+import styles from "./tabs.module.css";
+import { useState } from "react";
+import Description from "./Description";
+import MoreInfos from "./MoreInfos";
+// import Comments from "./Comments";
+const Tabs = ({ product }) => {
+  const [tab, setTab] = useState("description");
+  return (
+    <div data-aos="fade-left" className={styles.tabs}>
+      <input
+        onClick={() => setTab("description")}
+        type="radio"
+        id="description"
+        name="tab-control"
+        checked={tab == "description" && "checked"}
+      />
+      <input
+        onClick={() => setTab("moreInfos")}
+        type="radio"
+        id="moreInfos"
+        name="tab-control"
+        checked={tab == "moreInfos" && "checked"}
+      />
+      <input
+        onClick={() => setTab("comments")}
+        type="radio"
+        id="comments"
+        name="tab-control"
+        checked={tab == "comments" && "checked"}
+      />
+      <ul>
+        <li title="Features">
+          <label htmlFor="description" role="button">
+            {" "}
+            توضیحات{" "}
+          </label>
+        </li>
+        <li title="Delivery Contents">
+          <label htmlFor="moreInfos" role="button">
+            {" "}
+            اطلاعات بیشتر{" "}
+          </label>
+        </li>
+        <li title="Shipping">
+          <label htmlFor="comments" role="button">
+            {" "}
+            نظرات (
+            {
+              product.comments.filter((comment) => comment.isAccept).length
+            }){" "}
+          </label>
+        </li>
+      </ul>
+
+      <div className={styles.contents}>
+        <section className={styles.tabs_content}>
+          <Description />
+        </section>
+        <section className={styles.tabs_content}>
+          <MoreInfos product={JSON.parse(JSON.stringify(product))} />
+        </section>
+        <section className={styles.tabs_content}>
+          {/* <Comments
+            productID={product._id}
+            comments={JSON.parse(JSON.stringify(product.comments))}
+          /> */}
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Tabs;
