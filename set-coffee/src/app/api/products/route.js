92257c1a3e5da1e23740e1fb8b77c98d0902a8ad
Hamed Babaei <dev.hamedbabaei@gmail.com>
2024-04-03 21:25:28 +0330

    feat(product): develope add product api

---
+++ set-coffee/src/app/api/products/route.js
@@ -0,0 +1,42 @@
+import connectToDB from "@/configs/db";
+import ProductModel from "@/models/Product";
+
+export async function POST(req) {
+  try {
+    connectToDB();
+    const body = await req.json();
+    const {
+      name,
+      price,
+      shortDescription,
+      longDescription,
+      weight,
+      suitableFor,
+      smell,
+      tags,
+    } = body;
+
+    const product = await ProductModel.create({
+      name,
+      price,
+      shortDescription,
+      longDescription,
+      weight,
+      suitableFor,
+      smell,
+      tags,
+    });
+
+    return Response.json(
+      { message: "Product created successfully :))", data: product },
+      { status: 201 }
+    );
+  } catch (err) {
+    return Response.json({ message: err }, { status: 500 });
+  }
+}
+
+export async function GET() {
+  const products = await ProductModel.find({}, "-__v").populate("comments");
+  return Response.json(products);
+}
