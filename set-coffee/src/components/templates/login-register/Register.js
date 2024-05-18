9b9bd6b335a6cb5d7d766a040c2b621291cf13ef
Hamed Babaei <dev.hamedbabaei@gmail.com>
2024-05-19 02:29:36 +0330

    feat(otp): develope otp page and connect to client

--- set-coffee/src/components/templates/login-register/Register.js
+++ set-coffee/src/components/templates/login-register/Register.js
@@ -3,9 +3,9 @@ import styles from "./register.module.css";
 import Sms from "./Sms";
 import swal from "sweetalert";
 import { showSwal } from "@/utils/helpers";
-import { validateEmail, validatePassword, validatePhone } from "@/utils/auth";
+import { valiadteEmail, valiadtePassword, valiadtePhone } from "@/utils/auth";
 
-const Register = ({ showLoginForm }) => {
+const Register = ({ showloginForm }) => {
   const [isRegisterWithPass, setIsRegisterWithPass] = useState(false);
   const [isRegisterWithOtp, setIsRegisterWithOtp] = useState(false);
   const [name, setName] = useState("");
@@ -20,22 +20,22 @@ const Register = ({ showLoginForm }) => {
       return showSwal("نام را وارد بکنید", "error", "تلاش مجدد");
     }
 
-    const isValidPhone = validatePhone(phone);
+    const isValidPhone = valiadtePhone(phone);
     if (!isValidPhone) {
       return showSwal("شماره تماس وارد شده معتبر نیست", "error", "تلاش مجدد ");
     }
 
     if (email) {
-      const isValidEmail = validateEmail(email);
+      const isValidEmail = valiadteEmail(email);
       if (!isValidEmail) {
         return showSwal("ایمیل وارد شده معتبر نیست", "error", "تلاش مجدد ");
       }
     }
 
-    // const isValidPassword = validatePassword(password);
-    // if (!isValidPassword) {
-    //   return showSwal("پسورد وارد شده قابل حدس هست", "error", "تلاش مجدد ");
-    // }
+    const isValidPassword = valiadtePassword(password);
+    if (!isValidPassword) {
+      return showSwal("پسورد وارد شده قابل حدس هست", "error", "تلاش مجدد ");
+    }
 
     const user = { name, phone, email, password };
 
@@ -49,12 +49,41 @@ const Register = ({ showLoginForm }) => {
 
     if (res.status === 201) {
       showSwal("ثبت نام با موفقیت انجام شد", "success", "ورود به پنل کاربری");
-      setName("");
-      setPhone("");
-      setEmail("");
-      setPassword("");
     } else if (res.status === 422) {
       showSwal("کاربری با این اطلاعات از قبل وجود دارد", "error", "تلاش مجدد");
+    }
+  };
+
+  const sendOtp = async () => {
+    const isValidPhone = valiadtePhone(phone);
+    if (!isValidPhone) {
+      return showSwal("شماره تماس وارد شده معتبر نیست", "error", "تلاش مجدد ");
+    }
+
+    const res = await fetch("/api/auth/sms/send", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ phone }),
+    });
+
+    if (res.status === 201) {
+      swal({
+        title: "کد ورود با موفقیت Sms شد",
+        icon: "success",
+        buttons: "وارد کردن کد لاگین",
+      }).then(() => {
+        setIsRegisterWithOtp(true);
+      });
+    } else if (res.status === 422) {
+      swal({
+        title: "این شماره تماس قبلا ثبت نام شده",
+        icon: "error",
+        buttons: "لاگین می‌کنم",
+      }).then(() => {
+        showloginForm();
+      });
     }
   };
 
@@ -98,7 +127,7 @@ const Register = ({ showLoginForm }) => {
             <p
               style={{ marginTop: "1rem" }}
               className={styles.btn}
-              onClick={() => setIsRegisterWithOtp(true)}
+              onClick={sendOtp}
             >
               ثبت نام با کد تایید
             </p>
@@ -116,14 +145,14 @@ const Register = ({ showLoginForm }) => {
             >
               ثبت نام با رمزعبور
             </button>
-            <p onClick={showLoginForm} className={styles.back_to_login}>
+            <p onClick={showloginForm} className={styles.back_to_login}>
               برگشت به ورود
             </p>
           </div>
           <p className={styles.redirect_to_home}>لغو</p>
         </>
       ) : (
-        <Sms hideOtpForm={hideOtpForm} />
+        <Sms hideOtpForm={hideOtpForm} phone={phone} />
       )}
     </>
   );
