0bba2a309a8cc2902318148ce8038596aad823d5
Hamed Babaei <dev.hamedbabaei@gmail.com>
2024-05-21 01:27:07 +0330

    feat(products): develope image uploader api

--- set-coffee/src/app/api/products/route.js
+++ set-coffee/src/app/api/products/route.js
@@ -1,20 +1,28 @@
 import connectToDB from "@/configs/db";
 import ProductModel from "@/models/Product";
+// import fs from "fs";
+import { writeFile } from "fs/promises";
+import path from "path";
 
 export async function POST(req) {
   try {
     connectToDB();
-    const body = await req.json();
-    const {
-      name,
-      price,
-      shortDescription,
-      longDescription,
-      weight,
-      suitableFor,
-      smell,
-      tags,
-    } = body;
+    const formData = await req.formData();
+    const name = formData.get("name");
+    const price = formData.get("price");
+    const shortDescription = formData.get("shortDescription");
+    const longDescription = formData.get("longDescription");
+    const weight = formData.get("weight");
+    const suitableFor = formData.get("suitableFor");
+    const smell = formData.get("smell");
+    const tags = formData.get("tags");
+    const img = formData.get("img");
+
+    const buffer = Buffer.from(await img.arrayBuffer());
+    const filename = Date.now() + img.name;
+    const imgPath = path.join(process.cwd(), "public/uploads/" + filename);
+
+    await writeFile(imgPath, buffer);
 
     const product = await ProductModel.create({
       name,
@@ -25,6 +33,7 @@ export async function POST(req) {
       suitableFor,
       smell,
       tags,
+      img: `http://localhost:3000/uploads/${filename}`,
     });
 
     return Response.json(
@@ -32,8 +41,40 @@ export async function POST(req) {
       { status: 201 }
     );
   } catch (err) {
-    console.log("err in create new comments => ", err);
     return Response.json({ message: err }, { status: 500 });
+  }
+}
+
+// Image Uploader
+export async function PUT(req) {
+  const formData = await req.formData();
+  const img = formData.get("img");
+
+  // Validation
+  if (!img) {
+    return Response.json(
+      { message: "Product has not image !!" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const buffer = Buffer.from(await img.arrayBuffer());
+    const filename = Date.now() + img.name;
+
+    await writeFile(
+      path.join(process.cwd(), "public/uploads/" + filename),
+      buffer
+    );
+
+    // ✅
+    return Response.json(
+      { message: "File uploaded successfully :))" },
+      { status: 201 }
+    );
+  } catch (err) {
+    console.log(err);
+    return Response.json({ message: err.message }, { status: 500 });
   }
 }
 
